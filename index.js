c9c12eb4f84e2b6a5dff2f81a4b63f3e827bf02a
Vaughn Iverson <vsivsi@yahoo.com>
2014-03-31 18:20:29 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,278 @@
+// gridfs-locking-stream
+
+/**
+ * Module dependencies.
+ */
+
+var Lock = require('gridfs-locks').Lock;
+var LockCollection = require('gridfs-locks').LockCollection;
+
+var GridWriteStream = require('gridfs-stream').GridReadStream;
+var GridReadStream = require('gridfs-stream').GridWriteStream;
+
+/**
+ * Grid constructor
+ *
+ * @param {mongo.Db} db - an open mongo.Db instance
+ * @param {mongo} [mongo] - the native driver you are using
+ * @param {String} [root] - the root name of the GridFS collection to use
+ */
+
+function Grid (db, mongo, root) {
+  if (!(this instanceof Grid)) {
+    return new Grid(db, mongo, root);
+  }
+  var self = this;
+
+  mongo || (mongo = Grid.mongo ? Grid.mongo : undefined);
+
+  if (!mongo) throw new Error('missing mongo argument\nnew Grid(db, mongo)');
+  if (!db) throw new Error('missing db argument\nnew Grid(db, mongo)');
+
+  // the db must already be open b/c there is no `open` event emitted
+  // in old versions of the driver
+  self.db = db;
+  self.mongo = mongo;
+  self.root = root || self.mongo.GridStore.DEFAULT_ROOT_COLLECTION;
+}
+
+/**
+ * Creates a writable stream.
+ *
+ * @param {Object} [options]
+ * @return Stream
+ */
+
+Grid.prototype.createLockCollection = function (options, callback) {
+  var self = this;
+  self.root = options.root;
+  LockCollection.create(self.db, self.root, options, function (err, lockColl) {
+    if (err) { return callback(err); }
+    self._locks = lockColl;
+    callback(null);
+  });
+}
+
+/**
+ * Creates a writable stream.
+ *
+ * @param {Object} [options]
+ * @return Stream
+ */
+
+Grid.prototype.createWriteStream = function (options, callback) {
+  var self = this;
+
+  function lockAndWrite() {
+    var lock = new Lock(options._id, self._locks, options);
+    lock.obtainWriteLock(function (err, l) {
+      if (err) { return callback(err); }
+      if (!l) { return callback(null, null); }
+      var stream = new GridWriteStream(self, options);
+      _gridfs_lock = lock;
+      stream.releaseLock = function (callback) {
+        lock.releaseLock(callback || function () {});
+      }
+      stream.renewLock = function (callback) {
+        lock.renewLock(callback || function () {});
+      }
+      stream.on('error', function (err) {
+        lock.releaseLock(function (err) {
+          if (err) {
+            console.warn('Warning! releaseLock() failed for GridFS ' + self.root + " _id " + options._id);
+          }
+        });
+      }).on('close', function (file) {
+        lock.releaseLock(function (err) {
+          if (err) {
+            console.warn('Warning! releaseWriteLock() failed for GridFS ' + self.root + " _id " + options._id);
+          }
+        });
+      });
+      callback(null, stream, l);
+    });
+  }
+
+  if (!options._id) {
+    // New file
+    options._id = new self.mongo.BSONPure.ObjectID
+  }
+
+  if (options.root && self.root !== options.root) {
+    throw new Error('Root name of Grid object cannot be changed: ' + options.root + " !== " + self.root);
+  }
+
+  options.root = self.root;
+
+  if (!self._locks) {
+    this.createLockCollection(options, function(err) {
+      if (err) { return callback(err); }
+      lockAndWrite();
+    });
+  } else {
+    lockAndWrite();
+  }
+}
+
+/**
+ * Creates a readable stream. Pass at least a filename or _id option
+ *
+ * @param {Object} options
+ * @param {function} callback
+ * @return Stream
+ */
+
+Grid.prototype.createReadStream = function (options, callback) {
+  var self = this;
+
+  function lockAndRead() {
+    var lock = new Lock(options._id, self._locks, options);
+    lock.obtainReadLock(function (err, l) {
+      if (err) { return callback(err); }
+      if (!l) { return callback(null, null); }
+      var stream = new GridReadStream(self, options);
+      stream._gridfs_lock = lock;
+      stream.releaseLock = function (callback) {
+        lock.releaseLock(callback || function () {});
+      }
+      stream.renewLock = function (callback) {
+        lock.renewLock(callback || function () {});
+      }
+      stream.on('error', function (err) {
+        lock.releaseLock(function (err) {
+          if (err) {
+            console.warn('Warning! releaseLock() failed for GridFS ' + self.root + " _id " + options._id);
+          }
+        });
+      }).on('end', function (file) {
+        lock.releaseLock(function (err) {
+          if (err) {
+            console.warn('Warning! releaseLock() failed for GridFS ' + self.root + " _id " + options._id);
+          }
+        });
+      });
+      callback(null, stream, l);
+    });
+  }
+
+  if (!options._id) {
+    throw new Error('No "_id" provided for GridFS file. Filenames are not unique.');
+  }
+
+  if (options.root && self.root !== options.root) {
+    throw new Error('Root name of Grid object cannot be changed: ' + options.root + " !== " + self.root);
+  }
+
+  options.root = self.root;
+
+  if (!self._locks) {
+    this.createLockCollection(options, function(err) {
+      if (err) { return callback(err); }
+      lockAndRead();
+    });
+  } else {
+    lockAndRead();
+  }
+}
+
+/**
+ * The collection used to store file data in mongodb.
+ * @return {Collection}
+ */
+
+Object.defineProperty(Grid.prototype, 'files', {
+  get: function () {
+    var self = this;
+    if (self._col) return self._col;
+    return self._col = self.db.collection(self.root + ".files");
+  }
+});
+
+/**
+ * The collection used to store lock data in mongodb.
+ * @return {Collection}
+ */
+
+// Add a property for the locks collection?  Probably.
+
+Object.defineProperty(Grid.prototype, 'locks', {
+  get: function () {
+    var self = this;
+    if (self._locks) {
+      return self._locks.collection;
+    } else {
+      return self.db.collection(root + ".locks");
+    }
+  }
+});
+
+/**
+ * Removes a file by passing any options with an _id
+ *
+ * @param {Object} options
+ * @param {Function} callback
+ */
+
+Grid.prototype.remove = function (options, callback) {
+  var self = this;
+
+  if (!options._id) {
+    throw new Error('No "_id" provided for GridFS file. Filenames are not unique.');
+  }
+  var _id = self.tryParseObjectId(options._id) || options._id;
+
+  function lockAndRemove() {
+    var lock = new Lock(options._id, self._locks, options);
+    lock.obtainWriteLock(function (err, l) {
+      if (err) { return callback(err); }
+      if (!l) {
+        return callback(new Error('Remove failed due to failure to obtain write lock\nGridFS ' + self.root + " _id " + options._id));
+      }
+      self.mongo.GridStore.unlink(self.db, _id, options, function (err) {
+        lock.releaseLock(function (err2) {
+          if (err2) {
+            console.warn('Warning! releaseLock() failed for GridFS ' + self.root + " _id " + options._id);
+          }
+          callback(err);
+        });
+      });
+    });
+  }
+
+  if (options.root && self.root !== options.root) {
+    throw new Error('Root name of Grid object cannot be changed: ' + options.root + " !== " + self.root);
+  }
+  options.root = self.root;
+
+  if (!self._locks) {
+    this.createLockCollection(options, function(err) {
+      if (err) { return callback(err); }
+      lockAndRemove();
+    });
+  } else {
+    lockAndRemove();
+  }
+}
+
+/**
+ * Attemps to parse `string` into an ObjectId
+ *
+ * @param {GridReadStream} self
+ * @param {String|ObjectId} string
+ * @return {ObjectId|Boolean}
+ */
+
+Grid.prototype.tryParseObjectId = function tryParseObjectId (string) {
+  var self = this;
+  try {
+    return new self.mongo.BSONPure.ObjectID(string);
+  } catch (_) {
+    return false;
+  }
+}
+
+/**
+ * expose
+ */
+
+module.exports = exports = Grid;
---
+++ test/index.js
@@ -0,0 +1,385 @@
+
+// fixture/logo.png
+var assert = require('assert')
+  , Stream = require('stream')
+  , fs = require('fs')
+  , mongo = require('mongodb')
+  , Grid = require('../')
+  , fixturesDir = __dirname + '/fixtures/'
+  , imgReadPath = __dirname + '/fixtures/mongo.png'
+  , txtReadPath = __dirname + '/fixtures/text.txt'
+  , server
+  , db
+
+
+describe('test', function(){
+  var id;
+  before(function (done) {
+    server = new mongo.Server('localhost', 27017);
+    db = new mongo.Db('gridstream_test', server, {w:1});
+    db.open(done)
+  });
+
+  describe('Grid', function () {
+    it('should be a function', function () {
+      assert('function' == typeof Grid);
+    });
+    it('should create instances without the new keyword', function () {
+      var x = Grid(2,3,4);
+      assert(x instanceof Grid);
+    });
+    it('should store the arguments', function () {
+      var x = new Grid(4, 5, 6);
+      assert.equal(x.db, 4);
+      assert.equal(x.mongo, 5);
+      assert.equal(x.root, 6);
+    });
+    it('should require mongo argument', function(){
+      assert.throws(function () {
+        new Grid(3)
+      }, /missing mongo argument/);
+    })
+    it('should require db argument', function(){
+      assert.throws(function () {
+        new Grid(null, 3)
+      }, /missing db argument/);
+    })
+    it('should default .root to the value of GridStore.DEFAULT_ROOT_COLLECTION', function(){
+      var x = Grid(db, mongo);
+      assert.equal(x.root, mongo.GridStore.DEFAULT_ROOT_COLLECTION);
+    })
+    describe('files', function(){
+      it('returns a collection', function(){
+        var g = new Grid(db, mongo);
+        assert(g.files instanceof mongo.Collection);
+      })
+    })
+    describe('locks', function(){
+      it('returns a collection', function(){
+        var g = new Grid(db, mongo);
+        assert(g.locks instanceof mongo.Collection);
+      })
+    })
+  });
+
+  describe('createWriteStream', function(){
+    it('should be a function', function () {
+      var x = Grid(1, mongo);
+      assert('function' == typeof x.createWriteStream);
+    });
+  })
+
+  describe('GridWriteStream', function(){
+    var g
+      , ws
+
+    before(function(done){
+      Grid.mongo = mongo;
+      g = Grid(db);
+      ws = null;
+      g.createWriteStream({ filename: 'logo.png' }, function(e, stream) { ws = stream; done(); });
+    });
+
+    it('should be an instance of Stream', function(){
+      assert(ws instanceof Stream);
+    })
+    it('should should be writable', function(){
+      assert(ws.writable);
+    });
+    it('should store the grid', function(){
+      assert(ws._grid == g)
+    });
+    it('should have an id', function(){
+      assert(ws.id)
+    })
+    it('id should be an ObjectId', function(){
+      assert(ws.id instanceof mongo.BSONPure.ObjectID);
+    });
+    it('should have a name', function(){
+      assert(ws.name == 'logo.png')
+    })
+    describe('options', function(){
+      it('should have six keys', function(){
+        console.log("Keys: " + Object.keys(ws.options))
+        assert(Object.keys(ws.options).length === 6);
+      });
+      it('limit should be Infinity', function(){
+        assert(ws.options.limit === Infinity)
+      });
+    })
+    it('mode should default to w+', function(){
+      assert(ws.mode == 'w+');
+    })
+    it('should have an empty q', function(){
+      assert(Array.isArray(ws._q));
+      assert(ws._q.length === 0);
+    })
+    describe('store', function(){
+      it('should be an instance of mongo.GridStore', function(){
+        assert(ws._store instanceof mongo.GridStore)
+      })
+    })
+    describe('#methods', function(){
+      describe('write', function(){
+        it('should be a function', function(){
+          assert('function' == typeof ws.write)
+        })
+      })
+      describe('end', function(){
+        it('should be a function', function(){
+          assert('function' == typeof ws.end)
+        })
+      })
+      describe('destroy', function(){
+        it('should be a function', function(){
+          assert('function' == typeof ws.destroy)
+        })
+      })
+      describe('destroySoon', function(){
+        it('should be a function', function(){
+          assert('function' == typeof ws.destroySoon)
+        })
+      })
+    });
+    it('should provide piping from a readableStream into GridFS', function(done){
+      var readStream = fs.createReadStream(imgReadPath, { bufferSize: 1024 });
+      g.createWriteStream({ filename: 'logo.png'}, function(e, ws) {
+        // used in readable stream test
+        id = ws.id;
+
+        var progress = 0;
+
+        ws.on('progress', function (size) {
+          progress = size;
+        });
+
+        ws.on('close', function () {
+          assert(progress > 0);
+          done();
+        });
+        var pipe = readStream.pipe(ws);
+      });
+    });
+    it('should provide Error and File object on WriteStream close event', function(done){
+      var readStream = fs.createReadStream(imgReadPath, { bufferSize: 1024 });
+      var ws = g.createWriteStream({
+        mode: 'w',
+        filename: 'closeEvent.png',
+        content_type: "image/png"
+      }, function (e, ws) {
+        // used in readable stream test
+        id = ws.id;
+
+        var progress = 0;
+
+        ws.on('progress', function (size) {
+          progress = size;
+        });
+
+        ws.on('close', function (file) {
+          assert(file.filename === 'closeEvent.png')
+          assert(file.contentType === 'image/png')
+          assert(progress > 0);
+          done();
+        });
+        var pipe = readStream.pipe(ws);
+      });
+    });
+
+    it('should pipe more data to an existing GridFS file', function(done){
+      function pipe (id, cb) {
+        if (!cb) cb = id, id = null;
+        var readStream = fs.createReadStream(txtReadPath);
+        g.createWriteStream({
+          _id: id,
+          mode: 'w+' },
+          function (e, ws) {
+            ws.on('close', function () {
+              cb(ws.id);
+            });
+            readStream.pipe(ws);
+          });
+      }
+
+      pipe(function (id) {
+        pipe(id, function (id) {
+          // read the file out. it should consist of two copies of original
+          mongo.GridStore.read(db, id, function (err, txt) {
+            if (err) return done(err);
+            assert.equal(txt.length, fs.readFileSync(txtReadPath).length*2);
+            done();
+          });
+        });
+      })
+   });
+
+    it('should be able to store a 12-letter file name', function(done) {
+      g.createWriteStream({ filename: '12345678.png' }, function(e, rs) {
+        assert.equal(rs.name,'12345678.png');
+        done();
+      });
+    });
+  });
+
+  describe('createReadStream', function(){
+    it('should be a function', function () {
+      var x = Grid(1);
+      assert('function' == typeof x.createReadStream);
+    });
+  });
+
+  describe('GridReadStream', function(){
+    var g
+      , rs
+
+    before(function(done){
+      g = Grid(db);
+      g.createReadStream({
+        _id: id,
+        filename: 'logo.png'
+      }, function(e, stream) {
+        rs = stream;
+        done();
+      });
+    });
+
+    it('should create an instance of Stream', function(){
+      assert(rs instanceof Stream);
+    });
+    it('should should be readable', function(){
+      assert(rs.readable);
+    });
+    it('should store the grid', function(){
+      assert(rs._grid == g);
+    });
+    it('should have a name', function(){
+      assert(rs.name == 'logo.png');
+    })
+    it('should have an id', function(){
+      assert.equal(rs.id, id);
+    })
+    describe('options', function(){
+      it('should have three keys', function(done) {
+        g.createReadStream({ _id: id }, function(e, rs) {
+          assert(Object.keys(rs.options).length === 3);
+          done();
+        });
+      });
+    })
+    it('mode should default to r', function(){
+      assert(rs.mode == 'r');
+      assert(rs._store.mode == 'r');
+    })
+
+    describe('store', function(){
+      it('should be an instance of mongo.GridStore', function(){
+        assert(rs._store instanceof mongo.GridStore)
+      })
+    })
+    describe('#methods', function(){
+      describe('setEncoding', function(){
+        it('should be a function', function(){
+          assert('function' == typeof rs.setEncoding)
+          // TODO test actual encodings
+        })
+      })
+      describe('pause', function(){
+        it('should be a function', function(){
+          assert('function' == typeof rs.pause)
+        })
+      })
+      describe('destroy', function(){
+        it('should be a function', function(){
+          assert('function' == typeof rs.destroy)
+        })
+      })
+      describe('resume', function(){
+        it('should be a function', function(){
+          assert('function' == typeof rs.resume)
+        })
+      })
+      describe('pipe', function(){
+        it('should be a function', function(){
+          assert('function' == typeof rs.pipe)
+        })
+      })
+    });
+    it('should provide piping to a writable stream by id', function(done){
+      var file = fixturesDir + 'byid.png';
+      g.createReadStream({
+        _id: id
+      }, function(e, rs) {
+        var writeStream = fs.createWriteStream(file);
+        assert(rs.id instanceof mongo.BSONPure.ObjectID);
+        assert(rs.id == String(id))
+
+        var opened = false;
+        var ended = false;
+
+        rs.on('open', function () {
+          opened = true;
+        });
+
+        rs.on('error', function (err) {
+          throw err;
+        });
+
+        rs.on('end', function () {
+          ended = true;
+        });
+
+        writeStream.on('close', function () {
+           //check they are identical
+          assert(opened);
+          assert(ended);
+
+          var buf1 = fs.readFileSync(imgReadPath);
+          var buf2 = fs.readFileSync(file);
+
+          assert(buf1.length === buf2.length);
+
+          for (var i = 0, len = buf1.length; i < len; ++i) {
+            assert(buf1[i] == buf2[i]);
+          }
+
+          fs.unlinkSync(file);
+          done();
+        });
+
+        rs.pipe(writeStream);
+
+      });
+    });
+
+    it('should be possible to pause a stream after constructing it', function (done) {
+      g.createReadStream({ _id: id }, function (e, rs) {
+        rs.pause();
+        setTimeout(function () {
+          rs.resume();
+        }, 1000);
+
+        rs.on('data', function (data) {
+          done();
+        });
+      });
+    });
+
+    it('should allow removing files', function(done){
+      g.remove({ _id: id }, function (err) {
+        if (err) return done(err);
+        g.files.findOne({ _id: id }, function (err, doc) {
+          if (err) return done(err);
+          assert.ok(!doc);
+          done();
+        })
+      });
+    })
+
+  });
+
+  after(function (done) {
+    db.dropDatabase(function () {
+      db.close(true, done);
+    });
+  });
+});
